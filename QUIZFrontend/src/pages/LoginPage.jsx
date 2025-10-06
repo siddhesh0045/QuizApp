57e72f3cf0d747f35034777721ddf13863f03ff7
siddhesh0045 <123052009+siddhesh0045@users.noreply.github.com>
2025-10-06 18:47:48 +0530

    Update LoginPage.jsx
    Fix: SignUp case mismatch for Render build"

--- QUIZFrontend/src/pages/LoginPage.jsx
+++ QUIZFrontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Login from "../components/Login";
-import Signup from "../components/SignUp";
+import Signup from "../components/Signup";
 
 const LoginPage = () => {
   const [showSignup, setShowSignup] = useState(false);
